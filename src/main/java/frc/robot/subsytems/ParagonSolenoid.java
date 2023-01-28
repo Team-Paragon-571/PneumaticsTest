cc234a16c94b23091b591bba12aa0b1d11eb4ea5
Vaughn Glanville <vrglanny@gmail.com>
2023-01-28 14:36:46 -0500

    Add toggle method to solenoid
    - Added compressor idenifier to ParagonSolenoid
    - Added compressor variable to public ParagonSolenoid
    - Added a toggle function to manage the solenoid's state

--- src/main/java/frc/robot/subsytems/ParagonSolenoid.java
+++ src/main/java/frc/robot/subsytems/ParagonSolenoid.java
@@ -1,10 +1,7 @@
 package frc.robot.subsytems;
 
-import com.fasterxml.jackson.core.sym.Name;
-
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -12,13 +9,23 @@ public class ParagonSolenoid extends SubsystemBase {
     private final int m_can_id;
     private final int m_channel_id;
     private final Solenoid m_solenoid;
+    private final Pneumatics m_compressor;
 
-    public ParagonSolenoid(int can_id, int channel_id, PneumaticsModuleType module_type, String name) {
+    public ParagonSolenoid(int can_id, int channel_id, PneumaticsModuleType module_type, String name, Pneumatics compressor) {
         m_can_id = can_id;
         m_channel_id = channel_id;
         m_solenoid = new Solenoid(can_id, module_type, channel_id);
+        m_compressor = compressor;
 
         populate_shuffleboard(name);
+    }
+
+    public void toggle(){
+        if(m_compressor.is_enabled()){
+            m_solenoid.toggle();
+        } else {
+            m_solenoid.set(false);
+        }
     }
 
     private void populate_shuffleboard(String name) {
@@ -27,4 +34,5 @@ public class ParagonSolenoid extends SubsystemBase {
         SmartDashboard.putNumber("CAN ID", m_can_id);
         SmartDashboard.putNumber("Solenoid Channel ID", m_channel_id);
     }
+
 }
