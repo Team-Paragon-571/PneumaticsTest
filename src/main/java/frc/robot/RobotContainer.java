2de0b2dd3532c5fdb34c948144fc2c7efe51b6f7
Donomania <115245251+Donomania@users.noreply.github.com>
2023-01-28 16:42:52 -0500

    Initializes the compressor and the two solenoids

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,28 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsytems.ParagonSolenoid;
+import frc.robot.subsytems.Pneumatics;
 
 public class RobotContainer {
+  private final Pneumatics m_pneumatic;
+  private final ParagonSolenoid m_solenoidhands;
+  private final ParagonSolenoid m_solenoidbrake;
+
   public RobotContainer() {
+    int can_id = 8;
+    PneumaticsModuleType module_type = PneumaticsModuleType.CTREPCM;
+    int channel_id_hands = 0;
+    int channel_id_brake = 1;
+    String name_hands = "Hands";
+    String name_brake = "Brake";
+    m_pneumatic = new Pneumatics(can_id, module_type);
+    m_solenoidhands = new ParagonSolenoid(can_id, channel_id_hands, module_type, name_hands, m_pneumatic);
+    m_solenoidbrake = new ParagonSolenoid(can_id, channel_id_brake, module_type, name_brake, m_pneumatic);
+
     configureBindings();
   }
 
