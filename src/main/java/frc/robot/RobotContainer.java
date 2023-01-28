a5c752c35e9238609493754af00593dc9d46756e
pigcodes29 <103080686+pigcodes29@users.noreply.github.com>
2023-01-28 16:47:33 -0500

    Init subsystems; brakes, hands, and compressor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,26 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsytems.ParagonSolenoid;
+import frc.robot.subsytems.Pneumatics;
 
 public class RobotContainer {
-  public RobotContainer() {
-    configureBindings();
-  }
+  private final Pneumatics m_pneumatic;
+  private final ParagonSolenoid m_hands;
+  private final ParagonSolenoid m_brakes;
 
-  private void configureBindings() {
+  public RobotContainer() {
+    int can_id = 8;
+    int channel_id_hands = 0;
+    int channel_id_brakes = 1;
+    PneumaticsModuleType type = PneumaticsModuleType.CTREPCM;
+    m_pneumatic = new Pneumatics(can_id, type);
+    m_hands = new ParagonSolenoid(can_id, channel_id_hands, type, "hands", m_pneumatic);
+    m_brakes = new ParagonSolenoid(can_id, channel_id_brakes, type, "brakes", m_pneumatic);
+
   }
 
   public Command getAutonomousCommand() {
