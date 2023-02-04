067171b96d59201a0948f476099f745886cc1ff5
Vaughn Glanville <vrglanny@gmail.com>
2023-02-04 11:06:49 -0500

    Create a toggle method for ToggleBrakes
    - Finished defining the function "toggle()"
    - Added an override that returns true to prevent constant toggling
    - Added a list of requirements(HasSet) for the ToggleBrakes function
    - Added an execute to run the toggle method

--- src/main/java/frc/robot/commands/ToggleBrakes.java
+++ src/main/java/frc/robot/commands/ToggleBrakes.java
@@ -1,22 +1,36 @@
 package frc.robot.commands;
 
+import java.util.HashSet;
 import java.util.Set;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsytems.ParagonSolenoid;
 
-public class ToggleBrakes implements Command {
-    private final ParagonSolenoid m_brakes;
+public class ToggleBrakes extends CommandBase {
+    public final ParagonSolenoid m_brakes;
 
-    public ToggleBrakes (ParagonSolenoid brakes) {
+    public ToggleBrakes(ParagonSolenoid brakes) {
         m_brakes = brakes;
     }
 
     @Override
-    public Set<Subsystem> getRequirements() {
-        
-        return null;
+    public void execute() {
+        m_brakes.toggle();
     }
-    
-}   
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    public Set<Subsystem> getRequirements() {
+        HashSet<Subsystem> requirements = new HashSet<>();
+
+        requirements.add(m_brakes);
+
+        return requirements;
+    }
+
+}
